3584ff369d9de8f549b04f9d86450f1af1022407
Diwakar Dhamala <diwakardhamala04@gmail.com>
2023-12-19 12:35:11 +0545

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learning/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,14 +14,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('Welcome'),
-          ),
-        ),
-      ),
-    );
+      home:HomePage(),
+      );
   }
 }
